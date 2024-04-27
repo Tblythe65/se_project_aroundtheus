d21c984ad38d785f81d8d25a76f2e285f95437ab
Taylor Blythe <t65blythe@yahoo.com>
2024-04-27 14:36:02 -0500

    made cards get info from js

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,7 @@ const initialCards = [
   },
 ];
 
+// Elements
 const profileEditBtn = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileEditClose = document.querySelector("#profile-edit-close-button");
@@ -35,11 +36,36 @@ const profileInputDescription = document.querySelector(
   "#profile-input-description"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+const cardListEl = document.querySelector(".cards__list");
 
+// Functions
 function closePopup() {
   profileEditModal.classList.remove("modal__opened");
 }
 
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+
+  cardImageEl.setAttribute("src", cardData.link);
+  cardImageEl.setAttribute("alt", cardData.name);
+  cardTitleEl.textContent = cardData.name;
+
+  return cardElement;
+}
+
+// Event Handlers
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileInputTitle.value;
+  profileDescription.textContent = profileInputDescription.value;
+  closePopup();
+}
+
+// Event listeners
 profileEditBtn.addEventListener("click", () => {
   profileInputTitle.value = profileTitle.textContent;
   profileInputDescription.value = profileDescription.textContent;
@@ -47,13 +73,12 @@ profileEditBtn.addEventListener("click", () => {
   profileEditModal.classList.add("modal__opened");
 });
 
-profileEditClose.addEventListener("click", () => {
-  closePopup();
-});
+profileEditClose.addEventListener("click", closePopup);
 
-profileEditForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  profileTitle.textContent = profileInputTitle.value;
-  profileDescription.textContent = profileInputDescription.value;
-  closePopup();
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+
+  cardListEl.append(cardElement);
 });
